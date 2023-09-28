87942187049a23e650e0df2eb86bb941d770133f
Ahmed Khaled <92623979+a7medk7aledak@users.noreply.github.com>
2023-09-28 19:56:12 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,32 +1,89 @@
-let theInput,Button,reposData;
-theInput = document.querySelector(".get-repos input")
-Button = document.querySelector("button")
-reposData = document.querySelector(".show-data")
-Button.onclick = function () {
-    if(theInput.value === "")
-        reposData.innerHTML = "Please Write Github Username";
-    else { 
-        fetch(`https://api.github.com/users/${theInput.value}/repos`)
-        .then((response)=>response.json())
-        .then((repos)=> {
-            reposData.innerHTML = "";
-            repos.forEach(repo => {
-                // create repos
-                let div = document.createElement("div");
-                div.innerHTML = repo.name;
-                // create URL
-                let theUrl = document.createElement("a");
-                theUrl.innerHTML = "Visit";
-                theUrl.href = `https://api.github.com/users/${theInput.value}/${repo.name}`
-                theUrl.setAttribute('target','_blank');
-                div.appendChild(theUrl)
-                // create stars
-                let stars = document.createElement('span')
-                stars.innerHTML = `stars ${repo.stargazers_count}`;
-                div.append(stars)
-                div.className = 'repo-box';
-                reposData.appendChild(div)
-            });
-        })
+let input = document.querySelector("input");
+let submit = document.querySelector(".add");
+let tasksDiv = document.querySelector(".tasks");
+
+// array of tasks
+let arr = [];
+// check if theres tasks in local strage 
+if (window.localStorage.getItem("tasks"))
+    arr = JSON.parse(localStorage.getItem("tasks"))
+    getFromLocalStrorage ();
+// Add Task
+submit.addEventListener("click" , function () {
+    if(input.value !== "") {
+        addTask(input.value);
+        input.value = "";
     }
+})
+input.addEventListener("keyup" , function (e) {
+    if (e.key === "Enter") {
+        if(input.value !== "") {
+            addTask(input.value);
+            input.value = "";
+        }
+    }
+
+})
+tasksDiv.addEventListener("click",(e)=> {
+    if(e.target.classList.contains("del"))
+    {
+        deleteTask(e.target.parentElement.getAttribute("data-id"))
+        e.target.parentElement.remove()
+    }
+    if(e.target.classList.contains("task")) {
+        CheangeStatusTask(e.target.getAttribute("data-id"))
+        e.target.classList.toggle("done")
+    }
+})
+
+function addTask(taskText) {
+    const task = {
+        id: Date.now(),
+        text : taskText,
+        status: false,
+    };
+    arr.push(task);
+    addElements(arr);
+    addToLocalStrorage(arr);
+}
+function addElements () {
+    tasksDiv.innerHTML = "";
+    arr.forEach((task)=> {
+        let div = document.createElement("div");
+        div.className = "task";
+        // check if Task is Done
+        if (task.status)
+            div.className = "task done";
+        div.setAttribute("data-id",task.id);
+        div.append(document.createTextNode(task.text));
+        //create delete buttom
+        let span = document.createElement("span");
+        span.className = "del"
+        span.innerHTML = "Delete";
+        div.append(span);
+        tasksDiv.append(div);
+    })
+} 
+function addToLocalStrorage(arrOfTasks) {
+    window.localStorage.setItem("tasks", JSON.stringify(arrOfTasks))
+}
+function getFromLocalStrorage () {
+    let data = window.localStorage.getItem("tasks");
+    if (data) {
+        addElements(JSON.parse(data))
+    }
+}
+
+function deleteTask (taskId) {
+    arr = arr.filter((task)=> task.id != taskId)
+    addToLocalStrorage(arr)
+}
+
+function CheangeStatusTask (taskId) {
+    for(let i=0; i<arr.length;i++) {
+        if (arr[i].id==taskId) {
+            arr[i].status == false ? (arr[i].status = true) : (arr[i].status = false);
+        }
+    }
+      addToLocalStrorage(arr);  
 }
